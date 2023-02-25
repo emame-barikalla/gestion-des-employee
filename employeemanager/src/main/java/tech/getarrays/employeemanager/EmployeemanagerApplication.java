e00e829c085210f0794a991a336b867c3d61f9d3
emame barikalla <92339736+emame-barikalla@users.noreply.github.com>
2023-02-25 18:59:08 +0100

    Add files via upload

---
+++ employeemanager/src/main/java/tech/getarrays/employeemanager/EmployeemanagerApplication.java
@@ -0,0 +1,18 @@
+package tech.getarrays.employeemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class EmployeemanagerApplication  {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(EmployeemanagerApplication.class, args);
+		System.out.println("hello");
+
+
+	}
+
+}
